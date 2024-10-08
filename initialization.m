e4503f45895d1909d48a5431de587c5687408da0
XingyanLiu-Ethan <124031162+lxy0068@users.noreply.github.com>
2024-10-08 17:32:16 +0800

    Update initialization.m

--- initialization.m
+++ initialization.m
@@ -1,16 +1,14 @@
-
-% This function initialize the first population of search agents
+% 该函数初始化搜索代理的初始种群
 function Positions=initialization(SearchAgents_no,dim,ub,lb)
 
-Boundary_no= size(ub,1); % numnber of boundaries
+Boundary_no= size(ub,1); % 边界数量
 
-% If the boundaries of all variables are equal and user enter a signle
-% number for both ub and lb
+% 如果所有变量的边界相等且用户输入单个值作为ub和lb
 if Boundary_no==1
     Positions=rand(SearchAgents_no,dim).*(ub-lb)+lb;
 end
 
-% If each variable has a different lb and ub
+% 如果每个变量有不同的lb和ub
 if Boundary_no>1
     for i=1:dim
         ub_i=ub(i);
